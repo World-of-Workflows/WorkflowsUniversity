ea54636936af413db1137ecb84bacb4a962a6822
Nick Beaugeard <nick@releasedgroup.com>
2024-07-29 10:41:44 +1000

    Removing spaces in filenames

---
+++ CreateDocs.ps1
@@ -0,0 +1,9 @@
+# Creates Markdown Documents for Each Chapter
+# Then Consolidates them into a single PDF and a single DOCX
+
+# Index.md (No Change)
+# About
+pandoc -t markdown -o 01_about.md ./docs/01_about/readme.md ./docs/01_about/versions.md
+
+# Installation
+pandoc -t markdown -o 02_Installation.md ./docs/02_Installation/README.md './docs/02_Installation/World of Workflows PE on Windows.md'
